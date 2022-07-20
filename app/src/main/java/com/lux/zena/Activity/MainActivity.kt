07507e40ac84df2dfe6bf66e81e3a53970364368
lux <couquedas14@gmail.com>
2022-07-20 14:01:11 +0900

    text to speech

--- app/src/main/java/com/lux/zena/Activity/MainActivity.kt
+++ app/src/main/java/com/lux/zena/Activity/MainActivity.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeechService
 import android.util.Log
 import androidx.loader.content.CursorLoader
 import com.google.mlkit.vision.common.InputImage
@@ -17,6 +19,7 @@ import com.lux.zena.Activity.databinding.ActivityMainBinding
 import gun0912.tedimagepicker.builder.TedImagePicker
 import gun0912.tedimagepicker.builder.type.MediaType
 import java.io.InputStream
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +27,9 @@ class MainActivity : AppCompatActivity() {
     private val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     lateinit var mlText:String
     lateinit var image: InputImage
+
+    lateinit var tts:TextToSpeech
+
 
     fun getRealPathFromUri(uri:Uri) : String{
         val proj= arrayOf(MediaStore.Images.Media.DATA)
@@ -74,14 +80,33 @@ class MainActivity : AppCompatActivity() {
                     Log.e("RESULT","${result.result}")
                     binding.tv.text=result.result.toString()
                 }else Log.e("TASK","is not yet complete")
+        }
 
+        tts = TextToSpeech(this, TextToSpeech.OnInitListener { status->
+            if (status == TextToSpeech.SUCCESS){
+                tts.language = Locale.UK
+            }
+        })
 
+        binding.btnTts1.setOnClickListener {
+            tts.setPitch(1.0f)
+            tts.setSpeechRate(1.0f)
+            tts.speak(mlText,TextToSpeech.QUEUE_FLUSH, null)
+        }
 
-
+        binding.btnTts2.setOnClickListener {
+            tts.setPitch(2.0f)
+            tts.setSpeechRate(1.0f)
+            tts.speak(mlText,TextToSpeech.QUEUE_FLUSH,null)
         }
 
 
+
+
+
     }
+
+
 
 
 }
